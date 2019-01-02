b4413c09311cd56ec05ffbd000bb59db27527a6d
leojune <593682855@163.com>
2019-01-02 11:59:04 +0800

    展示内容一点修改

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,7 @@
 * 今天是报告年终总结的日子
 * 设计不会设计，视频也不会视频
 * 只能靠写写代码来完成这次总结了
-* 说做就做，我们就来一起写一份总结
+* 说做就做，就让我们就来一起写一份总结
 * 顺便也会展示一些我们前端在日常工作
 * 中的是什么样的
 */
@@ -41,7 +41,7 @@
 html {
   background: rgb(63, 82, 99);
   color: rgb(222,222,222);
-  font-size: 18px;
+  font-size: 20px;
 }
 /* 文字离边框太近了，我们来离远一点 */
 .styleEditor {
@@ -57,26 +57,30 @@ html {
 .token.punctuation{ color: yellow; }
 .token.function{ color: rgb(42,161,152); }
 
-/* 再来加一点 3D 效果 */
+/* emmm,这个我平时的工作环境还是有点差别,再加 */
+.styleEditor {
+  background-color: rgb(0, 0, 0);
+  border: 1px solid rgb(204, 204, 204);
+}
+/* 现在舒服了,再来加一点 3D 效果 */
 html{
   perspective: 1000px;
 }
 .styleEditor {
-  background-color: rgb(0, 0, 0);
-  border: 1px solid rgb(204, 204, 204);
   box-shadow: rgba(0, 0, 0, 0.3) -4px 4px 2px 0px;
   -webkit-transform: rotateY(10deg) translateZ(-100px) ;
           transform: rotateY(10deg) translateZ(-100px) ;
 }
-/* 代码部分的样式就准备完毕，不能忘了今天的主体 
+/* 代码部分的样式就准备完毕，不能忘了今天的主题 
  * 接下来我们来准备一个写总结的区域 
  */
 .summaryEditor{
   position: fixed; right: 0; top: 0;
   padding: .5em;  margin: .5em;
   width: 48vw; height: 90vh;
-  border: 1px solid;
   background: white; color: #222;
+  border: 1px solid rgb(0, 0, 0);
+  box-shadow: rgba(0, 0, 0, 0.3) -4px 4px 2px 0px;
   overflow: auto;
   -webkit-transform: rotateY(-10deg) translateZ(-100px) ;
           transform: rotateY(-10deg) translateZ(-100px) ;
@@ -88,7 +92,7 @@ html{
  * 在写样式之前，我们还是先把总结的样式写好
  * 要不然两个区域来回切换是在麻烦
  * ~~~~~~~~~~~~
- * 虽然真实的场景就是来两个区域来回切换，囧
+ * 虽然真实的场景就是来两个区域来回切换，囧 o(╯□╰)o
  */
 `
           ,
@@ -120,35 +124,35 @@ html{
 }
 .summaryEditor ol {
   counter-reset: section;
+  margin: 25px 0;
+  padding: 10px;
 }
 .summaryEditor ol li::before {
   counter-increment: section;
   content: counters(section, ".") " ";
   margin-right: .5em;
 }
-.summaryEditor blockquote {
-  margin: 1em;
-  padding: .5em;
-  background: #ddd;
-}
 /*
  * 好了，我们可以开始了
  * 这回是真的开始
+ * ヾ(◍°∇°◍)ﾉﾞ
+ * 不骗你
  */
 `],
         currentMarkdown: '',
         fullMarkdown: `# 1.2018年工作回顾
 ## 1. 2018年重点完成工作
 
- * 累计完成50个h5的制作，其中展示类10个，游戏类10个，视频类10个（视频类最让我头疼）
+ * 累计完成60+个h5的制作，其中展示类30个左右，游戏类15个左右，视频类15个左右（视频类最让我头疼）(粗略统计,大概比例是这样的把~~)
+ * 偶尔也客串下推文
  * 下午茶系统（看到自己写的东西不管什么原因在被很多人使用，这对于技术人员来说是一件令人高兴的事情）
- * 诶，没什么写了，所以说用ppt的话是在太少了 (⊙o⊙) …
+ * 诶，好像就这些了,没什么写了，所以说用ppt的话是在太少了 (⊙o⊙) …
  
 ## 2. 2018年 工作中存在的问题，经验总结
 
- * 有些h5在上线之后才发现bug，虽说要写没有bug的程序很难，但是只要在写的过程中逻辑在线，上线之前多测试，就能有效减少这类情况的出现
- * 有些合理需求由于自身技术原因，实现不了或实现的不够好，还是要保持学习的热情，提高自身姿势水平
- * 写h5过程中也会遇到莫名其妙的bug，还好部门同事都是见多识广，经验丰富的老手，也感谢在平时工作中他们对我的帮助
+  * 有些h5在上线之后才发现bug，虽说要写没有bug的程序很难，但是只要在写的过程中逻辑在线，上线之前多测试，就能有效减少这类情况的出现
+  * 有些合理需求由于自身技术原因，实现不了或实现的不够好，还是要保持学习的热情，提高自身姿势水平
+  * 写h5过程中也会遇到莫名其妙的bug，还好部门同事都是见多识广，经验丰富的老手，所以最终都能顺利解决,在这里也要感谢他们在平时工作中对我的帮助
 
   # 2. 2019年展望
 
@@ -156,10 +160,14 @@ html{
 
 1. 不在项目上线后才发现bug
 2. 提高自身姿势水平
-3. 成为解决方案的提供者，而不是问题的提供者
+3. 尽最大努力满足合理需求
+4. 成为解决方案的提供者，而不是问题的提供者
 
 
-好了，以上就是我的2018年年终总结，希望在2019年总结时能在上面的清单上都打上勾。
+好了，以上就是我的2018年年终总结，希望在2019年总结时能在上面的清单上都打上勾!
+
+by the way,大家有没有对我们前端的工作有增加那么一点点的了解呢?
+
 感谢大家观看！
 
 `
